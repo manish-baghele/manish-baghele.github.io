1f54e21c8228890afb4d9533b317f65940a057bc
manish <manish_baghele@yahoo.co.in>
2018-11-28 15:30:37 +0530

    change
    s

---
+++ multi_marker/scripts/events.js
@@ -0,0 +1,26 @@
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+                Idle='idle'
+                die ='rotate'
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                //const scale = aEntity.getAttribute('scale');
+                //Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.01);
+                aEntity.setAttribute('animation-mixer', { clip: die });
+                
+                    setTimeout(function(){
+
+                            aEntity.setAttribute('animation-mixer', { clip: Idle });
+                    }, 2000);
+            }
+        });
+}
+    
+
+
+});
